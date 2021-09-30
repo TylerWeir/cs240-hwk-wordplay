50ab10c7d07ac7ba88ce3876d3950ac00b8b4eed
Tyler Weir <tylerw@live.com>
2021-09-29 18:38:24 -1000

    Added a primitive random word selector function.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const ALPHABET_SIZE = 26;
 
-// Nodes for the trie data structure
-var trie_node = function() {
+// Constructor to make trie nodes 
+var trieNode = function() {
     this.children = new Array(ALPHABET_SIZE);
     this.isEndOfWord = false;
 }
 
-// Insert a new word into the trie.
-function add_word(rootNode, word) {
+// Insert a new word into a given trie.
+function addWord(rootNode, word) {
     var currentNode = rootNode;
 
     for (i = 0; i < word.length; i++) {
@@ -18,7 +18,7 @@ function add_word(rootNode, word) {
             // Make a node
             //console.log("making a new node");
             //console.log(String.fromCharCode(97+index));
-            currentNode.children[index] = new trie_node();
+            currentNode.children[index] = new trieNode();
             currentNode = currentNode.children[index];
         } else {
             // The node already exists
@@ -30,6 +30,8 @@ function add_word(rootNode, word) {
     currentNode.isEndOfWord = true;
 }
 
+// Searches the specified trie for the specified word. Returns true if the word
+// is present, false otherwise. 
 function search(rootNode, word) {
     var currentNode = rootNode;
 
@@ -45,10 +47,34 @@ function search(rootNode, word) {
     return currentNode.isEndOfWord;
 }
 
-var trie = new trie_node();
+// Returns a random word of specified length from a specified trie. 
+// 
+// This function is super slow when the word length surpasses 6!
+// This should be optimized. 
+//
+// This may be helpful: https://stackoverflow.com/questions/17152269/how-to-retrieve-a-random-word-of-a-given-length-from-a-trie
+function getRandomWord(rootNode, length) {
+    var word;
+    do {
+        var letterCode;
+        var letter;
+        word = "";
+
+        for(k = 0; k < length; k++){
+            letterCode = Math.floor(Math.random()*ALPHABET_SIZE + 97);
+            letter = String.fromCharCode(letterCode);
+            word = word.concat(letter);
+        }
+    } while (!search(rootNode, word));
+    
+    return word;
+}
+
+
+var trie = new trieNode();
 
 for (j = 0; j < dictionary.length; j++) {
-    add_word(trie, dictionary[j]);
+    addWord(trie, dictionary[j]);
 }
    
 console.log("done");
