c88aab4b56ca6b916ce3acc3aa1fd2079d33f53b
Tyler Weir <tylerw@live.com>
2021-09-29 18:07:00 -1000

    Finished the trie data structure.

--- app.js
+++ app.js
@@ -2,32 +2,54 @@ const ALPHABET_SIZE = 26;
 
 // Nodes for the trie data structure
 var trie_node = function() {
-    this.children = [null, null, null];
+    this.children = new Array(ALPHABET_SIZE);
     this.isEndOfWord = false;
 }
 
 // Insert a new word into the trie.
-function add_word(root_node, word) {
-    var currentNode = root_node;
+function add_word(rootNode, word) {
+    var currentNode = rootNode;
 
     for (i = 0; i < word.length; i++) {
-        var index = word[i] - 'a';
-        
+        // 97 is the integer value of a
+        var index = word.charCodeAt(i) - 97;
+        //console.log(index);
         if(currentNode.children[index] == null) {
             // Make a node
-            console.log("making a new node");
+            //console.log("making a new node");
+            //console.log(String.fromCharCode(97+index));
             currentNode.children[index] = new trie_node();
             currentNode = currentNode.children[index];
         } else {
             // The node already exists
             // Travel down
             currentNode = currentNode.children[index];
-            
         }
     }
+    //console.log("ADDED THE WORD");
     currentNode.isEndOfWord = true;
 }
 
+function search(rootNode, word) {
+    var currentNode = rootNode;
+
+    for (i = 0; i < word.length; i++) {
+        var index = word.charCodeAt(i) - 97;
+        
+        if(currentNode.children[index] == null) {
+            return false;
+        } else {
+            currentNode = currentNode.children[index];
+        }
+    }
+    return currentNode.isEndOfWord;
+}
 
 var trie = new trie_node();
-add_word(trie, "cat");
+
+for (j = 0; j < dictionary.length; j++) {
+    add_word(trie, dictionary[j]);
+}
+   
+console.log("done");
+   
